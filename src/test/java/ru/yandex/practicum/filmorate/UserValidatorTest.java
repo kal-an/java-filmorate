18b75670248776334e28018e86182d212227cbee
Andrey <dev.andr@yandex.com>
2022-04-21 12:27:15 +0500

    fix: оптимизировать тесты для сущности user

--- src/test/java/ru/yandex/practicum/filmorate/UserValidatorTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/UserValidatorTest.java
@@ -1,50 +1,43 @@
 package ru.yandex.practicum.filmorate;
 
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.validators.ValidatorService;
 
 import javax.validation.ConstraintViolationException;
 import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.time.format.DateTimeParseException;
 
 class UserValidatorTest {
 
     private static ValidatorService validator;
+    private static User user;
 
     @BeforeAll
     public static void setUp() {
         validator = new ValidatorService();
     }
 
-    @Test
-    @DisplayName("Пользователь с корректными данными")
+    @BeforeEach
+    @DisplayName("Создание сущности пользователь")
     public void createUser() {
-        User user = User.builder()
+        user = User.builder()
                 .login("dolore")
                 .name("est adipisicing")
                 .email("mail@mail.ru")
                 .birthday(LocalDate.of(1946, 8, 20))
                 .build();
+    }
+
+    @Test
+    @DisplayName("Пользователь с корректными данными")
+    public void createUserGoodFields() {
         Assertions.assertTrue(validator.isValid(user));
     }
 
     @Test
-    @DisplayName("Пользователь с некорректным логином")
-    public void createUserBadLogin() {
-        User user = User.builder()
-                .login("dolore ullamco")
-                .name("est adipisicing")
-                .email("mail@mail.ru")
-                .birthday(LocalDate.of(1946, 8, 20))
-                .build();
+    @DisplayName("Пользователь с пробелом в логине")
+    public void createUserLoginWithSpace() {
+        user.setLogin("dolore ullamco");
         ConstraintViolationException ex = Assertions.assertThrows(
                 ConstraintViolationException.class,
                 () -> validator.isValid(user)
@@ -56,12 +49,7 @@ class UserValidatorTest {
     @Test
     @DisplayName("Пользователь с пустым логином")
     public void createUserEmptyLogin() {
-        User user = User.builder()
-                .login("")
-                .name("est adipisicing")
-                .email("mail@mail.ru")
-                .birthday(LocalDate.of(1946, 8, 20))
-                .build();
+        user.setLogin("");
         ConstraintViolationException ex = Assertions.assertThrows(
                 ConstraintViolationException.class,
                 () -> validator.isValid(user)
@@ -73,12 +61,7 @@ class UserValidatorTest {
     @Test
     @DisplayName("Пользователь с значением логина null")
     public void createUserNullLogin() {
-        User user = User.builder()
-                .login(null)
-                .name("est adipisicing")
-                .email("mail@mail.ru")
-                .birthday(LocalDate.of(1946, 8, 20))
-                .build();
+        user.setLogin(null);
         ConstraintViolationException ex = Assertions.assertThrows(
                 ConstraintViolationException.class,
                 () -> validator.isValid(user)
@@ -89,12 +72,7 @@ class UserValidatorTest {
     @Test
     @DisplayName("Пользователь с некорректным email")
     public void createUserBadEmail() {
-        User user = User.builder()
-                .login("dolore")
-                .name("est adipisicing")
-                .email("mail.ru")
-                .birthday(LocalDate.of(1946, 8, 20))
-                .build();
+        user.setEmail("mail.ru");
         ConstraintViolationException ex = Assertions.assertThrows(
                 ConstraintViolationException.class,
                 () -> validator.isValid(user)
@@ -106,12 +84,7 @@ class UserValidatorTest {
     @Test
     @DisplayName("Пользователь с пустым email")
     public void createUserEmptyEmail() {
-        User user = User.builder()
-                .login("dolore")
-                .name("est adipisicing")
-                .email("")
-                .birthday(LocalDate.of(1946, 8, 20))
-                .build();
+        user.setEmail("");
         ConstraintViolationException ex = Assertions.assertThrows(
                 ConstraintViolationException.class,
                 () -> validator.isValid(user)
@@ -123,12 +96,7 @@ class UserValidatorTest {
     @Test
     @DisplayName("Пользователь с значением email null")
     public void createUserNullEmail() {
-        User user = User.builder()
-                .login("dolore")
-                .name("est adipisicing")
-                .email(null)
-                .birthday(LocalDate.of(1946, 8, 20))
-                .build();
+        user.setEmail(null);
         ConstraintViolationException ex = Assertions.assertThrows(
                 ConstraintViolationException.class,
                 () -> validator.isValid(user)
@@ -140,12 +108,7 @@ class UserValidatorTest {
     @Test
     @DisplayName("Пользователь с email из пробела")
     public void createUserSpaceEmail() {
-        User user = User.builder()
-                .login("dolore")
-                .name("est adipisicing")
-                .email(" ")
-                .birthday(LocalDate.of(1946, 8, 20))
-                .build();
+        user.setEmail(" ");
         ConstraintViolationException ex = Assertions.assertThrows(
                 ConstraintViolationException.class,
                 () -> validator.isValid(user)
@@ -157,12 +120,7 @@ class UserValidatorTest {
     @Test
     @DisplayName("Пользователь с днем рождения из будущего")
     public void createUserFutureBirthday() {
-        User user = User.builder()
-                .login("dolore")
-                .name("est adipisicing")
-                .email("mail@mail.ru")
-                .birthday(LocalDate.of(2446, 8, 20))
-                .build();
+        user.setBirthday(LocalDate.of(2446, 8, 20));
         ConstraintViolationException ex = Assertions.assertThrows(
                 ConstraintViolationException.class,
                 () -> validator.isValid(user)
@@ -174,39 +132,12 @@ class UserValidatorTest {
     @Test
     @DisplayName("Пользователь с пустой датой рождения")
     public void createUserEmptyBirthday() {
-        User user = User.builder()
-                .login("dolore")
-                .name("est adipisicing")
-                .email("mail@mail.ru")
-                .birthday(null)
-                .build();
+        user.setBirthday(null);
         ConstraintViolationException ex = Assertions.assertThrows(
                 ConstraintViolationException.class,
                 () -> validator.isValid(user)
         );
         Assertions.assertEquals("birthday: Дата рождения не может быть пустой.",
-                ex.getMessage());
-    }
-
-    @Test
-    @DisplayName("Пользователь с неверным форматом даты")
-    public void createUserBadBirthday() {
-        final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-        String json = "{\n" +
-                "  \"login\": \"dolore\",\n" +
-                "  \"name\": \"est adipisicing\",\n" +
-                "  \"email\": \"mail@mail.ru\",\n" +
-                "  \"birthday\": \"1946-30-30\"\n" +
-                "}";
-        JsonElement jsonElement = JsonParser.parseString(json);
-        JsonObject jsonObject = jsonElement.getAsJsonObject();
-        String birthday = jsonObject.get("birthday").getAsString();
-        DateTimeParseException ex = Assertions.assertThrows(
-                DateTimeParseException.class,
-                () -> LocalDate.parse(birthday, formatter)
-        );
-        Assertions.assertEquals("Text '1946-30-30' could not be parsed: " +
-                        "Invalid value for MonthOfYear (valid values 1 - 12): 30",
                 ex.getMessage());
     }
 }
