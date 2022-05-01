5b0688ad03d634d645241bcabad94470b6f88df9
Andrey <dev.andr@yandex.com>
2022-05-01 23:20:26 +0500

    fix: Перенести функционал EntityController

--- src/main/java/ru/yandex/practicum/filmorate/controllers/EntityController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controllers/EntityController.java
@@ -2,7 +2,7 @@ package ru.yandex.practicum.filmorate.controllers;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestBody;
-import ru.yandex.practicum.filmorate.exceptions.InvalidFilmException;
+import ru.yandex.practicum.filmorate.exceptions.InvalidEntityException;
 import ru.yandex.practicum.filmorate.model.Entity;
 
 import javax.validation.Valid;
@@ -13,33 +13,17 @@ import java.util.Map;
 @Slf4j
 abstract class EntityController<T extends Entity> {
 
-    private final Map<Integer, T> entities = new HashMap<>();
-
     public void create(@Valid @RequestBody T entity) {
         if (entity.getId() != null) {
             log.error(entity.toString());
-            throw new InvalidFilmException("Идентификатор должен быть пустым");
+            throw new InvalidEntityException("Идентификатор должен быть пустым");
         }
-        int index = entities.size() + 1;
-        entity.setId(index);
-        log.debug(entity.toString());
-        entities.put(index, entity);
     }
 
     public void update(@Valid @RequestBody T entity) {
         if (entity.getId() == null) {
             log.error(entity.toString());
-            throw new InvalidFilmException("Идентификатор не может быть пустым");
+            throw new InvalidEntityException("Идентификатор не может быть пустым");
         }
-        if (!entities.containsKey(entity.getId())) {
-            log.error(entity.toString());
-            throw new InvalidFilmException("Идентификатор не корректен");
-        }
-        log.debug(entity.toString());
-        entities.put(entity.getId(), entity);
-    }
-
-    public Collection<T> find() {
-        return entities.values();
     }
 }
