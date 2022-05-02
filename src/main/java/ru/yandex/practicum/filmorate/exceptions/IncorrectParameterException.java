e4aaa6edf5d037c8530788aea19bf6f1419dd38e
Andrey <dev.andr@yandex.com>
2022-05-02 17:53:59 +0500

    feat: Добавить классы для исключений

---
+++ src/main/java/ru/yandex/practicum/filmorate/exceptions/IncorrectParameterException.java
@@ -0,0 +1,8 @@
+package ru.yandex.practicum.filmorate.exceptions;
+
+public class IncorrectParameterException extends RuntimeException {
+
+    public IncorrectParameterException(String message) {
+        super(message);
+    }
+}
