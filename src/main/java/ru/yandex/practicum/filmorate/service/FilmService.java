8e583767921c698b6b9aad8bb5b3fc47d1b2ef55
Andrey <dev.andr@yandex.com>
2022-05-02 11:37:54 +0500

    fix: Добавить вызовы для работы с хранилищем

--- src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
@@ -2,8 +2,11 @@ package ru.yandex.practicum.filmorate.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.storage.film.FilmStorage;
 import ru.yandex.practicum.filmorate.storage.film.InMemoryFilmStorage;
+
+import java.util.Collection;
 
 @Service
 public class FilmService {
@@ -15,15 +18,31 @@ public class FilmService {
         this.storage = storage;
     }
 
+    public void addToStorage(Film film) {
+        storage.create(film);
+    }
+
+    public void updateInStorage(Film film) {
+        storage.update(film);
+    }
+
+    public void deleteFromStorage(Integer id) {
+        storage.delete(id);
+    }
+
+    public Collection<Film> getFromStorage() {
+        return storage.getEntities();
+    }
+
+    public Film findFilmById(Integer id) {
+        return storage.getEntity(id);
+    }
+
     public void addLike() {
 
     }
 
     public void deleteLike() {
-
-    }
-
-    public void getPopularFilms() {
 
     }
 }
--- src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
@@ -2,8 +2,12 @@ package ru.yandex.practicum.filmorate.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.storage.user.InMemoryUserStorage;
 import ru.yandex.practicum.filmorate.storage.user.UserStorage;
+
+import java.util.Collection;
 
 @Service
 public class UserService {
@@ -13,6 +17,22 @@ public class UserService {
     @Autowired
     public UserService(InMemoryUserStorage storage) {
         this.storage = storage;
+    }
+
+    public void addToStorage(User user) {
+        storage.create(user);
+    }
+
+    public void updateInStorage(User user) {
+        storage.update(user);
+    }
+
+    public Collection<User> getFromStorage() {
+        return storage.getEntities();
+    }
+
+    public User findUserById(Integer id) {
+        return storage.getEntity(id);
     }
 
     public void addFriend() {
