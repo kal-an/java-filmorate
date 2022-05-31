c46a6ff1a4329009b294ae3ff9997995fe8b12a9
Andrey <dev.andr@yandex.com>
2022-05-31 13:13:38 +0500

    feat: Добавить конструктор к User

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -3,12 +3,15 @@ package ru.yandex.practicum.filmorate.model;
 import lombok.*;
 import org.springframework.format.annotation.DateTimeFormat;
 
-import javax.validation.constraints.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Pattern;
 import java.time.LocalDate;
 import java.util.Set;
 
 @Builder
 @AllArgsConstructor
+@NoArgsConstructor
 @Getter
 @Setter
 @ToString
@@ -28,4 +31,12 @@ public class User extends Entity {
     private LocalDate birthday;
     private Set<Integer> friends;
     private Set<Integer> likedFilms;
+
+    public User(Integer id, String login, String name, String email, LocalDate birthday) {
+        this.setId(id);
+        this.login = login;
+        this.name = name;
+        this.email = email;
+        this.birthday = birthday;
+    }
 }
