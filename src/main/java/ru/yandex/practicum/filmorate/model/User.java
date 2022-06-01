f6652f18862b7b955bb4d2e4bc666047dc7df71c
Andrey <dev.andr@yandex.com>
2022-06-01 10:33:53 +0500

    fix: Доработать класс User

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -7,6 +7,7 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Pattern;
 import java.time.LocalDate;
+import java.util.HashSet;
 import java.util.Set;
 
 @Builder
@@ -29,8 +30,8 @@ public class User extends Entity {
     @Past
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate birthday;
-    private Set<Integer> friends;
-    private Set<Integer> likedFilms;
+    private Set<Integer> friends = new HashSet<>();
+    private Set<Integer> likedFilms = new HashSet<>();
 
     public User(Integer id, String login, String name, String email, LocalDate birthday) {
         this.setId(id);
