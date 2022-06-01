654596ed4e61c0faeb34a6b0f4d575c06225ac8a
Andrey <dev.andr@yandex.com>
2022-06-01 15:13:17 +0500

    feat: Добавить класс Mpa

---
+++ src/main/java/ru/yandex/practicum/filmorate/model/Mpa.java
@@ -0,0 +1,24 @@
+package ru.yandex.practicum.filmorate.model;
+
+public enum Mpa {
+    G(1, "G"),
+    PG(2, "PG"),
+    PG13(3, "PG-13"),
+    R(4, "R"),
+    NC17(5, "NC-17");
+    private Integer id;
+    private String name;
+
+    private Mpa(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
