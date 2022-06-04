8c10e63d8e7fef16a6ce5612bc810f4fd22edcb6
Andrey <dev.andr@yandex.com>
2022-06-04 14:54:11 +0500

    feat: Добавить метод getPopularFilm в интерфейс FilmStorage

--- src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmStorage.java
@@ -3,5 +3,9 @@ package ru.yandex.practicum.filmorate.storage.film;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.storage.EntityStorage;
 
+import java.util.List;
+
 public interface FilmStorage extends EntityStorage<Film> {
+
+    List<Film> getPopularFilm(Integer size);
 }
