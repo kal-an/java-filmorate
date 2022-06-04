412ff528ec179a690eac386dba7d738e6fbfbd6d
Andrey <dev.andr@yandex.com>
2022-06-04 14:50:08 +0500

    fix: Доработать интерфейс UserStorage

--- src/main/java/ru/yandex/practicum/filmorate/storage/user/UserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/user/UserStorage.java
@@ -3,5 +3,19 @@ package ru.yandex.practicum.filmorate.storage.user;
 import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.storage.EntityStorage;
 
+import java.util.List;
+
 public interface UserStorage extends EntityStorage<User> {
+
+    List<User> getUserFriends(Integer id);
+
+    List<User> getCommonFriends(Integer id, Integer otherId);
+
+    void deleteFriend(Integer id, Integer otherId);
+
+    void addFriend(Integer id, Integer otherId);
+
+    void addLike(Integer userId, Integer filmId);
+
+    void deleteLike(Integer userId, Integer filmId);
 }
