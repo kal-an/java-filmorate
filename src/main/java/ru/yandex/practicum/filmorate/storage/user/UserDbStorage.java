facfcd96d85a4e83357344a85f9d7348ff30cc27
Andrey <dev.andr@yandex.com>
2022-06-04 22:53:01 +0500

    fix: Убрать лишний импорт

--- src/main/java/ru/yandex/practicum/filmorate/storage/user/UserDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/user/UserDbStorage.java
@@ -16,7 +16,6 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.time.LocalDate;
 import java.util.Collection;
-import java.util.List;
 import java.util.Optional;
 
 @Repository
