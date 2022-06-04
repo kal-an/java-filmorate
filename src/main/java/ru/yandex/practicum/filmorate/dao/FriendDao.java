9250805572fc05077d52337ff1139c24ffa14507
Andrey <dev.andr@yandex.com>
2022-06-04 14:49:38 +0500

    feat: Реализовать класс UserDbStorage

--- src/main/java/ru/yandex/practicum/filmorate/dao/FriendDao.java
+++
@@ -1,8 +0,0 @@
-package ru.yandex.practicum.filmorate.dao;
-
-import java.util.Set;
-
-public interface FriendDao {
-
-    Set<Integer> getFriendsId(Integer id);
-}
--- src/main/java/ru/yandex/practicum/filmorate/storage/user/UserDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/user/UserDbStorage.java
@@ -1,4 +1,4 @@
-package ru.yandex.practicum.filmorate.dao.impl;
+package ru.yandex.practicum.filmorate.storage.user;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -9,7 +9,6 @@ import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 import ru.yandex.practicum.filmorate.exceptions.UserNotFoundException;
 import ru.yandex.practicum.filmorate.model.User;
-import ru.yandex.practicum.filmorate.storage.user.UserStorage;
 
 import java.sql.Date;
 import java.sql.PreparedStatement;
@@ -17,16 +16,17 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.time.LocalDate;
 import java.util.Collection;
+import java.util.List;
 import java.util.Optional;
 
 @Repository
-public class UserDaoImpl implements UserStorage {
+public class UserDbStorage implements UserStorage {
 
     private final JdbcTemplate jdbcTemplate;
-    private final Logger log = LoggerFactory.getLogger(UserDaoImpl.class);
+    private final Logger log = LoggerFactory.getLogger(UserDbStorage.class);
 
     @Autowired
-    public UserDaoImpl(JdbcTemplate jdbcTemplate) {
+    public UserDbStorage(JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
     }
 
@@ -47,13 +47,14 @@ public class UserDaoImpl implements UserStorage {
 
     @Override
     public Collection<User> getEntities() {
-        String sql = "SELECT * FROM user";
+        String sql = "SELECT user_id, login, name, email, birthday FROM user";
         return jdbcTemplate.query(sql, (rs, rowNum) -> makeUser(rs));
     }
 
     @Override
     public Optional<User> getEntity(Integer id) {
-        String sql = "SELECT * FROM user WHERE user_id = ?";
+        String sql = "SELECT user_id, login, name, email, birthday " +
+                "FROM user WHERE user_id = ?";
         final Optional<User> optionalUser = jdbcTemplate
                 .query(sql, (rs, rowNum) -> makeUser(rs), id)
                 .stream()
@@ -90,6 +91,30 @@ public class UserDaoImpl implements UserStorage {
         jdbcTemplate.update(sql, id);
     }
 
+    @Override
+    public void deleteFriend(Integer id, Integer otherId) {
+        String sql = "DELETE FROM friend WHERE user_id = ? and friend_id = ?";
+        jdbcTemplate.update(sql, id, otherId);
+    }
+
+    @Override
+    public void addFriend(Integer id, Integer otherId) {
+        String sql = "INSERT INTO friend (user_id, friend_id) VALUES (?, ?)";
+        jdbcTemplate.update(sql, id, otherId);
+    }
+
+    @Override
+    public void addLike(Integer userId, Integer filmId) {
+        String sql = "INSERT INTO liked_film (user_id, film_id) VALUES (?, ?)";
+        jdbcTemplate.update(sql, userId, filmId);
+    }
+
+    @Override
+    public void deleteLike(Integer userId, Integer filmId) {
+        String sql = "DELETE FROM liked_film WHERE user_id ? and film_id = ?";
+        jdbcTemplate.update(sql, userId, filmId);
+    }
+
     private User makeUser(ResultSet rs) throws SQLException {
         Integer id = rs.getInt("user_id");
         String login = rs.getString("login");
@@ -97,5 +122,28 @@ public class UserDaoImpl implements UserStorage {
         String email = rs.getString("email");
         LocalDate birthday = rs.getDate("birthday").toLocalDate();
         return new User(id, login, name, email, birthday);
+    }
+
+    @Override
+    public List<User> getUserFriends(Integer id) {
+        String sql = "SELECT f.friend_id AS user_id, u.login, u.name, u.email, u.birthday " +
+                "FROM friend AS f " +
+                "INNER JOIN user AS u ON f.friend_id = u.user_id " +
+                "WHERE f.user_id = ?";
+        return jdbcTemplate.query(sql, (rs, rowNum) -> makeUser(rs), id);
+    }
+
+    @Override
+    public List<User> getCommonFriends(Integer id, Integer otherId) {
+        String sql = "SELECT f.friend_id AS user_id, u.login, u.name, u.email, u.birthday " +
+                "    FROM friend AS f " +
+                "    INNER JOIN user AS u ON f.friend_id = u.user_id " +
+                "    WHERE f.user_id = ? " +
+                "INTERSECT " +
+                "SELECT f.friend_id AS user_id, u.login, u.name, u.email, u.birthday " +
+                "    FROM friend AS f " +
+                "    INNER JOIN user AS u ON f.friend_id = u.user_id " +
+                "    WHERE f.user_id = ?";
+        return jdbcTemplate.query(sql, (rs, rowNum) -> makeUser(rs), id, otherId);
     }
 }
