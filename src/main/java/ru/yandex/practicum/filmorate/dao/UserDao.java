b4b329c754630eab942c676d5455893845deffa7
Andrey <dev.andr@yandex.com>
2022-06-04 12:11:35 +0500

    feat: Добавить метод deleteLike

--- src/main/java/ru/yandex/practicum/filmorate/dao/UserDao.java
+++ src/main/java/ru/yandex/practicum/filmorate/dao/UserDao.java
@@ -1,5 +1,6 @@
 package ru.yandex.practicum.filmorate.dao;
 
+import ru.yandex.practicum.filmorate.model.Like;
 import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.storage.EntityStorage;
 
@@ -12,4 +13,8 @@ public interface UserDao extends EntityStorage<User> {
     List<User> getCommonFriends(Integer id, Integer otherId);
 
     void deleteFriend(Integer id, Integer otherId);
+
+    Like addLike(Integer userId, Integer filmId);
+
+    void deleteLike(Integer userId, Integer filmId);
 }
