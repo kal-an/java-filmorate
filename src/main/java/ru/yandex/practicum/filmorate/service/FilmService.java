d8e35aab78a0d4a75bc373f17c2d724d0b0da5a5
Andrey <dev.andr@yandex.com>
2022-06-04 12:10:03 +0500

    fix: Доработать FilmService

--- src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
@@ -4,24 +4,29 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
+import ru.yandex.practicum.filmorate.dao.FilmDao;
 import ru.yandex.practicum.filmorate.exceptions.FilmNotFoundException;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.storage.film.FilmStorage;
 
-import java.util.*;
-import java.util.stream.Collectors;
+import java.util.Collection;
+import java.util.List;
+import java.util.Optional;
 
 @Slf4j
 @Service
 public class FilmService {
 
     private final FilmStorage storage;
+    private final FilmDao filmDao;
     private final UserService userService;
 
     @Autowired
-    public FilmService(@Qualifier("filmDbStorage") FilmStorage storage, UserService userService) {
+    public FilmService(@Qualifier("filmDaoImpl") FilmStorage storage,
+                       FilmDao filmDao, UserService userService) {
         this.storage = storage;
+        this.filmDao = filmDao;
         this.userService = userService;
     }
 
@@ -50,48 +55,30 @@ public class FilmService {
     public void addLike(Integer filmId, Integer userId) {
         final Optional<User> optionalUser = userService.findUserById(userId);
         final Optional<Film> optionalFilm = findFilmById(filmId);
-        if (optionalUser.isPresent()) {
+        if (optionalUser.isPresent() && optionalFilm.isPresent()) {
             final User user = optionalUser.get();
-            if (user.getLikedFilms() == null) {
-                user.setLikedFilms(new HashSet<>());
-            }
-            user.getLikedFilms().add(filmId);
-            userService.updateUser(user);
-            log.info(String.format("Пользователь %s поставил like", user));
-        }
-        if (optionalFilm.isPresent()) {
             final Film film = optionalFilm.get();
+            userService.addLike(userId, filmId);
             film.setRate(film.getRate() + 1);
             updateFilm(film);
-            log.info(String.format("К фильму %s добавлен like", film));
+            log.info("Пользователь {} поставить лайк фильму {} ", user, film);
         }
     }
 
     public void deleteLike(Integer filmId, Integer userId) {
         final Optional<User> optionalUser = userService.findUserById(userId);
         final Optional<Film> optionalFilm = findFilmById(filmId);
-        if (optionalUser.isPresent()) {
+        if (optionalUser.isPresent() && optionalFilm.isPresent()) {
             final User user = optionalUser.get();
-            if (user.getLikedFilms() == null) {
-                user.setLikedFilms(new HashSet<>());
-            }
-            user.getLikedFilms().remove(filmId);
-            userService.updateUser(user);
-            log.info(String.format("Пользователь %s удалил like", user));
-        }
-        if (optionalFilm.isPresent()) {
             final Film film = optionalFilm.get();
+            userService.deleteLike(userId, filmId);
             film.setRate(film.getRate() - 1);
             updateFilm(film);
-            log.info(String.format("У фильма %s удален like", film));
+            log.info("Пользователь {} удалил лайк у фильма {} ", user, film);
         }
     }
 
     public List<Film> getPopularFilm(Integer size) {
-        return storage.getEntities()
-                .stream()
-                .sorted(Comparator.comparing(Film::getRate).reversed())
-                .limit(size)
-                .collect(Collectors.toList());
+        return filmDao.getPopularFilm(size);
     }
 }
