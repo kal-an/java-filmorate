089500b9a8ea1e71b9deec0ef2ba98623de24301
Andrey <dev.andr@yandex.com>
2022-06-06 11:25:34 +0500

    fix: Перенести часть запросов в пакет для тестирования

--- src/test/resources/data.sql
+++ src/test/resources/data.sql
@@ -15,3 +15,21 @@ INSERT INTO GENRE (name) VALUES ( 'Мультфильм' );
 INSERT INTO GENRE (name) VALUES ( 'Триллер' );
 INSERT INTO GENRE (name) VALUES ( 'Документальный' );
 INSERT INTO GENRE (name) VALUES ( 'Боевик' );
+
+INSERT INTO user (login, name, email, birthday)
+    VALUES ( 'dolore', 'Nick Name', 'mail@mail.ru', '1946-08-20');
+INSERT INTO user (login, name, email, birthday)
+    VALUES ( 'friend', 'friend adipisicing', 'friend@mail.ru', '1976-08-20');
+INSERT INTO user (login, name, email, birthday)
+    VALUES ( 'common', 'col', 'friend@common.ru', '2000-08-20');
+
+INSERT INTO friend (user_id, friend_id) VALUES ( '1', '2' );
+INSERT INTO friend (user_id, friend_id) VALUES ( '1', '3' );
+INSERT INTO friend (user_id, friend_id) VALUES ( '2', '3' );
+
+INSERT INTO film (name, description, release_date, duration, rate, mpa_id)
+    VALUES ( 'labore nulla', 'Duis in consequat esse', '1979-04-17',  '100', '4', '1');
+INSERT INTO film (name, description, release_date, duration, rate, mpa_id)
+    VALUES ( 'New film', 'New film about friends', '1999-04-30', '120', '4', '3');
+
+INSERT INTO liked_film (USER_ID, FILM_ID) VALUES ( '1', '2' );
