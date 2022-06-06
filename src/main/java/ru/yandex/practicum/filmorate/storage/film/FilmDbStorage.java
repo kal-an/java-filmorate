c599ce90bc625a48e37d124b2470d3b82a50b93d
Andrey <dev.andr@yandex.com>
2022-06-06 14:18:59 +0500

    fix: Исправить запрос в методе getPopularFilm

--- src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmDbStorage.java
@@ -59,11 +59,15 @@ public class FilmDbStorage implements FilmStorage {
 
     @Override
     public Collection<Film> getPopularFilm(Integer size) {
-        String sql = "SELECT f.film_id, f.name, f.description, f.release_date, f.duration, " +
-                "f.rate, m.mpa_id, m.name AS mpa_name, m.description AS mpa_description " +
+        String sql = "SELECT f.film_id, f.name, f.description, f.release_date, f.duration, f.rate, " +
+                "m.mpa_id, m.name AS mpa_name, m.description AS mpa_description, COUNT(lf.film_id) AS count_likes " +
                 "FROM film AS f " +
                 "INNER JOIN mpa AS m ON m.mpa_id = f.mpa_id " +
-                "ORDER BY f.rate DESC LIMIT ?";
+                "LEFT JOIN liked_film lf ON f.film_id = lf.film_id " +
+                "GROUP BY f.film_id, f.name, f.description, f.release_date, f.duration, f.rate, " +
+                "m.mpa_id, mpa_name, mpa_description " +
+                "ORDER BY count_likes " +
+                "LIMIT ?";
         return jdbcTemplate.query(sql, (rs, rowNum) -> makeFilm(rs), size);
     }
 
